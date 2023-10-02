9310603aaffa3339e81fd3d0c88123735ac73deb
Agimir <william@mesaindigostudios.com>
2023-10-02 01:39:29 -0500

    yur

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+--[[    cfx.re script information    ]]--
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+
+--[[    resource information    ]]--
+name 'template'
+author 'username'
+version '1.0.0'
+repository 'https://github.com/username/template'
+description 'template for making scripts'
+
+--[[    dependency information    ]]--
+dependencies {
+
+}
+
+--[[    manifest information    ]]--
+files {
+
+}
+
+shared_scripts {
+
+}
+
+client_scripts {
+
+}
+
+server_scripts {
+
+}
